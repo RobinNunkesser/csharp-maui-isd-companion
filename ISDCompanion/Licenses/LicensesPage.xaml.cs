a2a258bf2ea419815a3e11c4cf66189c16ef0c6f
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-09-15 11:28:18 +0200

    licenses

--- ISDCompanion/Licenses/LicensesPage.xaml.cs
+++ ISDCompanion/Licenses/LicensesPage.xaml.cs
@@ -8,13 +8,13 @@ namespace ISDCompanion
 {
     public partial class LicensesPage : ContentPage
     {
-        public LicensesPage()
+        public LicensesPage(string licenseFile)
         {
             InitializeComponent();
 
             var assembly = typeof(LicensesPage).GetTypeInfo().Assembly;
             Stream stream = assembly
-                .GetManifestResourceStream("ISDCompanion.Settings.licenses.html");
+                .GetManifestResourceStream($"ISDCompanion.Licenses.{licenseFile}");
             string html = "";
             using (var reader = new System.IO.StreamReader(stream))
             {
