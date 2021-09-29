6d2719440094f34d3ba6ca9859ba4e244af55758
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-09-29 08:01:30 +0200

    Starting Quiz

--- ISDCompanion/Quiz/QuizPage.xaml.cs
+++ ISDCompanion/Quiz/QuizPage.xaml.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 
 using Xamarin.Forms;
 
-namespace ISDCompanion.Quiz
+namespace ISDCompanion
 {
     public partial class QuizPage : ContentPage
     {
         public QuizPage()
         {
             InitializeComponent();
+            BindingContext = new QuizViewModel();
+        }
+
+        async void Answer_Clicked(object sender, System.EventArgs e)
+        {
+            falseButton.IsEnabled = false;
+            trueButton.IsEnabled = false;
+            skipButton.IsEnabled = false;
+            await answerLabel.FadeTo(1, 1000);
+            await answerLabel.FadeTo(0, 200);
+            falseButton.IsEnabled = true;
+            trueButton.IsEnabled = true;
+            skipButton.IsEnabled = true;
         }
     }
 }
--- ISDCompanion/Quiz/QuizViewModel.cs
+++ ISDCompanion/Quiz/QuizViewModel.cs
@@ -1,16 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RandomExtensions;
 using TriviaPorts;
 
-namespace ISDCompanion.Quiz
+namespace ISDCompanion
 {
-    public class QuizViewModel
+    public class QuizViewModel : INotifyPropertyChanged
     {
         private readonly List<IQuestion> questions = new List<IQuestion> { new YesNoQuestion() { } };
+        private readonly IQuestion[] shuffledQuestions;
+        private Random _random = new();
 
         public QuizViewModel()
         {
+            questions.Add(new YesNoQuestion()
+            {
+                Answer = true,
+                Text = "Donkey"
+            });
+            shuffledQuestions = questions.ToArray();
+            _random.Shuffle(shuffledQuestions);
+            OnPropertyChanged(nameof(Question));
         }
+
+        public string Question => shuffledQuestions[index].Text;
+
+        private int index;
+        public int Index
+        {
+            get => index;
+            set
+            {
+                if (value != index)
+                {
+                    index = value;
+                    OnPropertyChanged(nameof(Question));
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+          PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        #endregion
 
         private class YesNoQuestion : IYesNoQuestion
         {
