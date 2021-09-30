e3bd23387ecafe0b29c83f7b5e8d27ed3f60088a
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-09-30 15:55:40 +0200

    Buddy

---
+++ ISDCompanion/Exercises/BuddyViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using OperatingSystemsExam.Buddy;
+using Xamarin.Forms;
+
+namespace ISDCompanion
+{
+    public class BuddyViewModel : INotifyPropertyChanged
+    {
+        public ICommand NewParams { get; set; }
+
+        private string requests;
+        public string Requests
+        {
+            get => requests;
+            set
+            {
+                if (value != requests)
+                {
+                    requests = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string freeOrder;
+        public string FreeOrder
+        {
+            get => freeOrder;
+            set
+            {
+                if (value != freeOrder)
+                {
+                    freeOrder = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string solution;
+        public string Solution
+        {
+            get => solution;
+            set
+            {
+                if (value != solution)
+                {
+                    solution = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public BuddyViewModel()
+        {
+            Initialize();
+            NewParams = new Command(Initialize);
+        }
+
+        private void Initialize()
+        {
+            var parameters = new BuddyParameters();
+            var solver = new BuddySolver();
+            var solution = solver.Solve(parameters);
+
+            var solutionString = "";
+
+            foreach (var entry in solution.History)
+            {
+                foreach (var cell in entry)
+                {
+                    solutionString += cell == -1 ? "-" : parameters.Processes[cell];
+                }
+                solutionString += "\n";
+            }
+
+            Requests = $"A ({parameters.Requests[0]}), B ({parameters.Requests[1]}), C ({parameters.Requests[2]}), D ({parameters.Requests[3]}), E ({parameters.Requests[4]})";
+            FreeOrder = $"Free {parameters.FreeOrder[0]}, {parameters.FreeOrder[1]}, {parameters.FreeOrder[2]}, {parameters.FreeOrder[3]}, {parameters.FreeOrder[4]}";
+            Solution = solutionString;
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+          PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        #endregion
+
+    }
+}
