1a79fda0cb63d1d0210eddb5e8c5b1ccc670700f
Alexander Strauch <alexander.strauch@stud.hshl.de>
2022-04-24 16:00:27 +0200

    Added Models for proper Navigation and property binding, changed exercise page to collapsible Stacklayout

--- ISDCompanion/Exercises/ExercisesViewModel.cs
+++ ISDCompanion/Exercises/ExercisesViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using ISDCompanion.Models;
 using Italbytz.Ports.Trivia;
 using Xamarin.Forms;
 
@@ -11,8 +14,14 @@ namespace ISDCompanion
         public ICommand NetworksQuizCommand { get; set; }
         public ICommand OpSysQuizCommand { get; set; }
 
+        public Command<Exercise> ExerciseTapped { get; }
+
         public ExercisesViewModel(INavigation navigation)
         {
+            _semesters = new ObservableCollection<Semester>();
+
+            PopulateData();
+
             NavigateCommand = new Command<Type>(async (Type pageType) =>
             {
                 Page page = (Page)Activator.CreateInstance(pageType);
@@ -26,6 +35,124 @@ namespace ISDCompanion
             {
                 await navigation.PushAsync(new QuizPage(Italbytz.Adapters.Exam.OperatingSystems.YesNoQuestions.Questions));
             });
+        }
+
+        private void PopulateData()
+        {
+            ObservableCollection<Exercise> networks = new ObservableCollection<Exercise>();
+
+            networks.Add(new Exercise
+            {
+                Exercise_Title = "Bitencodings",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:BitencodingsPage}"
+            });
+
+            networks.Add(new Exercise
+            {
+                Exercise_Title = "CRC",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:CRCPage}"
+            });
+
+            networks.Add(new Exercise
+            {
+                Exercise_Title = "MST",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:MSTPage}"
+            });
+
+            networks.Add(new Exercise
+            {
+                Exercise_Title = "ShortestPaths",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:ShortestPathsPage}"
+            });
+
+            networks.Add(new Exercise
+            {
+                Exercise_Title = "Netmasks",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:NetmaskPage}"
+            });
+
+
+
+            ObservableCollection<Exercise> opsys = new ObservableCollection<Exercise>();
+
+            opsys.Add(new Exercise
+            {
+                Exercise_Title = "PageReplacement",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:PageReplacementPage}"
+            });
+
+            opsys.Add(new Exercise
+            {
+                Exercise_Title = "Buddy",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:BuddyPage}"
+            });
+
+            opsys.Add(new Exercise
+            {
+                Exercise_Title = "Scheduling",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:SchedulingPage}"
+            });
+
+            opsys.Add(new Exercise
+            {
+                Exercise_Title = "RealtimeScheduling",
+                Command = "{Binding NavigateCommand}",
+                CommandParameter = "{x:Type local:RealtimeSchedulingPage}"
+            });
+
+
+
+            ObservableCollection<Topic> temp_Topics = new ObservableCollection<Topic>();
+
+            temp_Topics.Add(new Topic
+            {
+                Topic_Title = "Betriebssysteme",
+                Exercises = networks
+            }); 
+            
+            temp_Topics.Add(new Topic
+            {
+                Topic_Title = "Netzwerke",
+                Exercises = opsys
+            });
+
+            ObservableCollection<Module> temp_Modules = new ObservableCollection<Module>();
+
+            temp_Modules.Add(new Module
+            {
+                Module_Title = "Betriebssysteme & Netzwerke",
+                Topics = temp_Topics
+            }); 
+
+            _semesters.Add(new Semester
+            {
+                Semester_Title = "Fachsemester 4",
+                Modules = temp_Modules
+            }); 
+            
+            
+        }
+
+        private ObservableCollection<Semester> _semesters { get; set; }
+
+        public ObservableCollection<Semester> Semesters
+        {
+            get
+            {
+                return _semesters;
+            }
+            private set
+            {
+                _semesters = value;
+            }
         }
 
         private class YesNoQuestion : IYesNoQuestion
---
+++ ISDCompanion/Models/Module.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ISDCompanion.Models
+{
+    public class Module
+    {
+        public string Module_Title { get; set; }
+
+        public ObservableCollection<Topic> Topics { get; set; }
+    }
+}
