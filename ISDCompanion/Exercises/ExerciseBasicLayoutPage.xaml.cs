22871aa2d5075d197ce49bc0d93e16161e6da4ec
Lunamy <marcelifix@gmail.com>
2022-04-25 17:41:12 +0200

    Added Exercise Basic Layout Page to test the new Layout for the exercise-pages

---
+++ ISDCompanion/Exercises/ExerciseBasicLayoutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ISDCompanion
+{
+    public partial class ExerciseBasicLayoutPage : ContentPage
+    {
+        public ExerciseBasicLayoutPage()
+        {
+            InitializeComponent();
+            BindingContext = new ExerciseBasicLayoutViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Switch.IsToggled = false;
+        }
+    }
+}
