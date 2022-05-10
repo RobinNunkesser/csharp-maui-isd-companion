0aa31e91e4f75f560abcf17bfd8aab700a4e14a5
Marcel Felix <marcel.felix@stud.hshl.de>
2022-05-10 09:26:14 +0200

    Added FontAwesome Icons (test)

--- ISDCompanion/AssemblyInfo.cs
+++ ISDCompanion/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("FA-Regular-400.otf", Alias = "AC")]
+[assembly: ExportFont("FA-Solid-900.otf", Alias = "ACC")]
