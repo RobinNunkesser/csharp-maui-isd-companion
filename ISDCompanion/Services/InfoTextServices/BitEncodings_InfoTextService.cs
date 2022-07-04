caf161e5fb3f830edc95e51f89ae8f0595e2a2b5
Alexander Strauch <alexander.strauch@stud.hshl.de>
2022-07-04 16:39:48 +0200

    Implemented Table Generation for BitEncodings. InfoText still missing.

---
+++ ISDCompanion/Services/InfoTextServices/BitEncodings_InfoTextService.cs
@@ -0,0 +1,33 @@
+﻿using ISDCompanion.Resx;
+using ISDCompanion.Services.Interfaces;
+using Italbytz.Adapters.Exam.Networks;
+using Italbytz.Adapters.Exam.OperatingSystems;
+using Italbytz.Ports.Exam.Networks;
+using Italbytz.Ports.Exam.OperatingSystems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ISDCompanion.Services.InfoTextServices
+{
+    internal class BitEncodings_InfoTextService : IInfoTextService
+    {
+        BitencodingParameters _parameters;
+        IBitencodingSolution _solution;
+
+        public BitEncodings_InfoTextService(BitencodingParameters parameters, IBitencodingSolution solution)
+        {
+            _parameters = parameters;
+            _solution = solution;
+        }
+
+        public string[] GetInfoTexts()
+        {
+            string[] infoTexts = new string[_parameters.Bits.Length];
+            
+
+            return infoTexts;
+        }
+    }
+}
