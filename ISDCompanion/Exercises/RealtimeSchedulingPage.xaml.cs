3fe6145995e647e877c2c32d1f107f04773b699b
Marcel Felix <marcel.felix@stud.hshl.de>
2022-07-04 14:56:57 +0200

    Fixed ScrollView-Scroll in Exercises

--- ISDCompanion/Exercises/RealtimeSchedulingPage.xaml.cs
+++ ISDCompanion/Exercises/RealtimeSchedulingPage.xaml.cs
@@ -19,24 +19,10 @@ namespace ISDCompanion
             var vm = new RealtimeSchedulingViewModel();
             BindingContext = vm;
             ViewModelAfterRender = vm;
-            vm.ScrollToPosition += (int x, int y, bool isAnimated) => { ScrollToPosition(x, y, isAnimated); };
+            
+            vm.ScrollToPosition += (int x, int y, bool isAnimated) => { Content.ScrollToPosition(x, y, isAnimated); };
         }
 
-        private void ScrollToPosition(int x, int y, bool isAnimated)
-        {
-            //object scrollView = FindByName("scrollView");
-            //ScrollView scrollView2 = (ScrollView)scrollView;
 
-            //var animation = new Animation(
-            //    callback: x => scrollView2.ScrollToAsync(x, y, animated: false),
-            //    start: scrollView2.ScrollX,
-            //    end: x);
-
-            //animation.Commit(
-            //    owner: this,
-            //    name: "Scroll",
-            //    length: 300,
-            //    easing: Easing.SinInOut);
-        }
     }
 }
