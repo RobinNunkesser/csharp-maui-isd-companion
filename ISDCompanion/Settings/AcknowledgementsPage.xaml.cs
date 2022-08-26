5d9bbbb3c900f7be58794b952f2bec334792cf17
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-08-26 11:31:50 +0200

    Acknowledgements

---
+++ ISDCompanion/Settings/AcknowledgementsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ISDCompanion
+{
+    public partial class AcknowledgementsPage : ContentPage
+    {
+        public AcknowledgementsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
