2e60de4a1ed2c81a2f7a362ce3b4bff391bb5924
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-08-30 11:51:55 +0200

    Start of calendar export

---
+++ ISDCompanion/Schedule/CoursesPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using ISDCompanion.Resx;
+using Plugin.Calendars;
+using Plugin.Calendars.Abstractions;
+using Xamarin.Forms;
+
+namespace ISDCompanion
+{
+    public partial class CoursesPage : ContentPage
+    {
+        public CoursesPage()
+        {
+            InitializeComponent();
+            BindingContext = new CoursesViewModel();
+        }
+
+        async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string previous = (e.PreviousSelection.FirstOrDefault() as ItemViewModel)?.Text;
+            string current = (e.CurrentSelection.FirstOrDefault() as ItemViewModel)?.Text;
+
+            var calendars = await CrossCalendars.Current.GetCalendarsAsync();
+            var editableCalendars = calendars.Where((c) => c.CanEditCalendar).ToList();
+
+            var selectedCalendar = editableCalendars.FirstOrDefault();
+
+            if (editableCalendars.Count == 0)
+            {
+                await DisplayAlert(AppResources.Error, AppResources.NoCalendars, AppResources.OK);
+                return;
+            }
+            else if (editableCalendars.Count > 1)
+            {
+                var names = editableCalendars.Select((c) => c.Name).ToArray();
+                string chosenCalendar = await DisplayActionSheet("Kalender?", "Cancel", null, names);
+                if (chosenCalendar.Equals("Cancel"))
+                {
+                    return;
+                }
+                foreach (var calendar in editableCalendars)
+                {
+                    if (calendar.Name.Equals(chosenCalendar))
+                    {
+                        selectedCalendar = calendar;
+                        break;
+                    }
+                }
+            }
+
+
+            var calendarEvent = new CalendarEvent
+            {
+                Name = "Add calendar support",
+                Start = DateTime.Now,
+                End = DateTime.Now.AddHours(1),
+                Reminders = new List<CalendarEventReminder> { new CalendarEventReminder() }
+            };
+            await CrossCalendars.Current.AddOrUpdateEventAsync(selectedCalendar, calendarEvent);
+        }
+    }
+}
+
---
+++ ISDCompanion/Schedule/CoursesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ISDCompanion
+{
+    public class CoursesViewModel : ViewModel
+    {
+
+        public List<ItemViewModel> Items { get; set; } = new List<ItemViewModel>
+            {
+            new ItemViewModel { Text = "Sponge", Detail = "Detail 1" },
+            new ItemViewModel { Text = "Banana", Detail = "Detail 2" },
+            new ItemViewModel { Text = "Laptop", Detail = "Detail 3" },
+            new ItemViewModel { Text = "Teddy Bear", Detail = "Detail 4" }
+            };
+
+        public CoursesViewModel()
+        {
+        }
+    }
+
+    public class ItemViewModel
+    {
+        public string Text { get; set; }
+        public string Detail { get; set; }
+    }
+}
+
