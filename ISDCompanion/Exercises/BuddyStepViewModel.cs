96cfd588df15292cd7231384148182b02cd42e5f
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-12-14 14:47:33 +0100

    Buddy is working

--- ISDCompanion/Exercises/BuddyStepViewModel.cs
+++ ISDCompanion/Exercises/BuddyStepViewModel.cs
@@ -3,6 +3,7 @@ namespace ISDCompanion
 {
     public class BuddyStepViewModel : ExerciseStepViewModel
     {
+        public String Label { get; set; }
         public String[] Occupation { get; set; }
 
         public BuddyStepViewModel()
