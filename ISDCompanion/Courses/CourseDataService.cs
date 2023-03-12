76829f7ed532242d3fc37df67a287d1051eb0363
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-12 22:09:19 +0100

    New schedule

--- ISDCompanion/Courses/CourseDataService.cs
+++ ISDCompanion/Courses/CourseDataService.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using Java.Lang;
+using Java.Security;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using Plugin.Calendars;
 using Plugin.Calendars.Abstractions;
+using static Android.Provider.Settings;
 using Calendar = Plugin.Calendars.Abstractions.Calendar;
 
 namespace StudyCompanion
@@ -13,11 +17,11 @@ namespace StudyCompanion
         private static readonly string shortFormat = "dd.MM.yyyy";
         public static readonly string longFormat = "dd.MM.yyyy HH:mm";
 
-        public static DateTime SemesterEnd { get; } = DateTime.ParseExact("15.01.2023", shortFormat, CultureInfo.InvariantCulture);
+        public static DateTime SemesterEnd { get; } = DateTime.ParseExact("25.06.2023", shortFormat, CultureInfo.InvariantCulture);
 
         public static List<DateTime> Holidays { get; } = new List<DateTime>
         {
-            DateTime.ParseExact("03.10.2022",shortFormat, CultureInfo.InvariantCulture),
+            /*DateTime.ParseExact("03.10.2022",shortFormat, CultureInfo.InvariantCulture),
             DateTime.ParseExact("01.11.2022",shortFormat, CultureInfo.InvariantCulture),
             DateTime.ParseExact("24.12.2022",shortFormat, CultureInfo.InvariantCulture),
             DateTime.ParseExact("25.12.2022",shortFormat, CultureInfo.InvariantCulture),
@@ -27,12 +31,23 @@ namespace StudyCompanion
             DateTime.ParseExact("29.12.2022",shortFormat, CultureInfo.InvariantCulture),
             DateTime.ParseExact("30.12.2022",shortFormat, CultureInfo.InvariantCulture),
             DateTime.ParseExact("31.12.2022",shortFormat, CultureInfo.InvariantCulture),
-            DateTime.ParseExact("01.01.2023",shortFormat, CultureInfo.InvariantCulture)
+            DateTime.ParseExact("01.01.2023",shortFormat, CultureInfo.InvariantCulture),*/
+            DateTime.ParseExact("07.04.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("10.04.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("11.04.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("12.04.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("13.04.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("14.04.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("01.05.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("18.05.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("29.05.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("30.05.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("08.06.2023",shortFormat, CultureInfo.InvariantCulture)
         };
 
         public static List<CourseViewModel> Courses { get; } = new List<CourseViewModel>
             {
-            new CourseViewModel {
+            /*new CourseViewModel {
                 Semester = 1,
                 StartDate = "19.09.2022 08:00",
                 Name = "Chemie VL",
@@ -445,7 +460,379 @@ namespace StudyCompanion
                 Room = "Labor H3.3-E01-160",
                 Length = 120,
                 Biweekly = false
-            }
+            }*/
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "20.03.2023 08:00",
+                Name = "Praktikum Elektrotechnik",
+                Lecturer = "Glasmachers",
+                Room = "Labor H3.3-E01-150",
+                Length = 240,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "21.03.2023 08:00",
+                Name = "Personal Skills II VL",
+                Lecturer = "Grewe",
+                Room = "Seminarraum H1.1-E01-170",
+                Length = 120,
+                Biweekly = true
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "28.03.2023 08:00",
+                Name = "Personal Skills II ÜB",
+                Lecturer = "Grewe",
+                Room = "Seminarraum H1.1-E01-170",
+                Length = 120,
+                Biweekly = true
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "21.03.2023 10:00",
+                Name = "Mathematik II VL",
+                Lecturer = "Ponick",
+                Room = "Hörsaal HAM 7",
+                Length = 180,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "21.03.2023 14:00",
+                Name = "Technisches Englisch II VL+ÜB",
+                Lecturer = "Strack",
+                Room = "Seminarraum H4.3-E00-100",
+                Length = 180,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "22.03.2023 09:00",
+                Name = "Objektorientierte Programmierung VL",
+                Lecturer = "Stuckenholz",
+                Room = "Hörsaal HAM 4",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "22.03.2023 11:00",
+                Name = "Objektorientierte Programmierung ÜB",
+                Lecturer = "Stuckenholz",
+                Room = "PC-Pool H4.2-E00-140",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "22.03.2023 14:00",
+                Name = "System Modellierung I VL",
+                Lecturer = "Runovska",
+                Room = "Hörsaal HAM 7",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "23.03.2023 08:00",
+                Name = "Elektrotechnik VL",
+                Lecturer = "Glasmachers",
+                Room = "Hörsaal HAM 3",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "23.03.2023 10:00",
+                Name = "Elektrotechnik ÜB",
+                Lecturer = "Glasmachers",
+                Room = "Hörsaal HAM 3",
+                Length = 60,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "23.03.2023 12:00",
+                Name = "System Modellierung I ÜB",
+                Lecturer = "Runovska",
+                Room = "PC-Pool H4.2-E00-140",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "23.03.2023 14:00",
+                Name = "Mathematik II ÜB",
+                Lecturer = "Ponick",
+                Room = "Hörsaal WESTPRESS",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "24.03.2023 09:00",
+                Name = "Algorithmen und Datenstrukturen VL",
+                Lecturer = "Stuckenholz",
+                Room = "Seminarraum H1.1-E01-150",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 2,
+                StartDate = "24.03.2023 11:00",
+                Name = "Algorithmen und Datenstrukturen ÜB",
+                Lecturer = "Stuckenholz",
+                Room = "PC-Pool H4.2-E00-140",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "20.03.2023 11:00",
+                Name = "Software Design VL",
+                Lecturer = "Nunkesser",
+                Room = "Seminarraum H4.3-E00-110",
+                Length = 240,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "20.03.2023 13:00",
+                Name = "Computer Security VL",
+                Lecturer = "Pelzl",
+                Room = "Hörsaal HAM 6",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "20.03.2023 13:00",
+                Name = "Computer Security VL",
+                Lecturer = "Pelzl",
+                Room = "Hörsaal HAM 6",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "20.03.2023 17:00",
+                Name = "Computer Security ÜB",
+                Lecturer = "Pelzl",
+                Room = "Seminarraum H1.1-E01-150",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "21.03.2023 08:00",
+                Name = "Personal Skills IV VL",
+                Lecturer = "Zips",
+                Room = "Hörsaal Stadtwerke Hamm",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "21.03.2023 08:00",
+                Name = "Personal Skills IV VL",
+                Lecturer = "Zips",
+                Room = "Hörsaal Stadtwerke Hamm",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "21.03.2023 10:00",
+                Name = "Datenbanken VL",
+                Lecturer = "Grewe",
+                Room = "Seminarraum H7.2-E00-002",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "21.03.2023 12:00",
+                Name = "Datenbanken ÜB",
+                Lecturer = "Grewe",
+                Room = "PC-Pool H4.2-E00-140",
+                Length = 60,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "21.03.2023 14:00",
+                Name = "SSP I: Digitaltechnik II VL",
+                Lecturer = "Krenz-Baath",
+                Room = "Hörsaal HAM 3",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "22.03.2023 09:00",
+                Name = "SSP I: Embedded Systems II VL",
+                Lecturer = "Pelzl",
+                Room = "Seminarraum H7.2-E00-001",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "22.03.2023 12:00",
+                Name = "SSP I: Embedded Systems II ÜB",
+                Lecturer = "Pelzl",
+                Room = "Labor H3.3-E01-160",
+                Length = 120,
+                Biweekly = true
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "29.03.2023 12:00",
+                Name = "SSP I: Digitaltechnik II ÜB",
+                Lecturer = "Krenz-Baath",
+                Room = "Labor H3.3-E01-160",
+                Length = 120,
+                Biweekly = true
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "22.03.2023 12:00",
+                Name = "SSP I: App Frontends VL",
+                Lecturer = "Nunkesser",
+                Room = "Seminarraum H1.1-E01-130",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "22.03.2023 14:00",
+                Name = "Corporate Management VL",
+                Lecturer = "Thorn",
+                Room = "Hörsaal HAM 6",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "24.03.2023 09:00",
+                Name = "Software Design ÜB",
+                Lecturer = "Nunkesser",
+                Room = "Labor H3.3-E01-160",
+                Length = 240,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "24.03.2023 13:00",
+                Name = "SSP I: App Frontends ÜB",
+                Lecturer = "Nunkesser",
+                Room = "Labor H3.3-E01-160",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 4,
+                StartDate = "24.03.2023 16:00",
+                Name = "SSP I: Web Frontends VL",
+                Lecturer = "Stuckenholz",
+                Room = "Seminarraum H4.2-E00-110",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "20.03.2023 09:00",
+                Name = "SSP II: Mobile Security VL",
+                Lecturer = "Pelzl",
+                Room = "Labor H3.3-E01-160",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "20.03.2023 12:00",
+                Name = "Personal Skills V VL",
+                Lecturer = "Zips",
+                Room = "Seminarraum H1.1-E01-160",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "20.03.2023 14:00",
+                Name = "Personal Skills V ÜB",
+                Lecturer = "Zips",
+                Room = "Seminarraum H1.1-E01-160",
+                Length = 60,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "21.03.2023 10:00",
+                Name = "SSP II: Parallel Computing VL",
+                Lecturer = "Krenz-Baath",
+                Room = "Seminarraum H1.1-E01-150",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "21.03.2023 12:00",
+                Name = "SSP II: iOS Development VL",
+                Lecturer = "Nunkesser",
+                Room = "Seminarraum H1.1-E01-140",
+                Length = 90,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "21.03.2023 13:30",
+                Name = "SSP II: iOS Development ÜB",
+                Lecturer = "Nunkesser",
+                Room = "Labor H3.3-E01-220",
+                Length = 90,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "21.03.2023 14:00",
+                Name = "SSP II: Embedded Security VL",
+                Lecturer = "Pelzl",
+                Room = "Seminarraum H4.3-E00-110",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "22.03.2023 11:00",
+                Name = "Entrepreneurial Finance VL",
+                Lecturer = "Thorn",
+                Room = "Hörsaal HAM 4",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "22.03.2023 14:00",
+                Name = "Künstliche Intelligenz VL",
+                Lecturer = "Krenz-Baath",
+                Room = "Seminarraum H1.1-E01-150",
+                Length = 120,
+                Biweekly = false
+            },
+            new CourseViewModel {
+                Semester = 6,
+                StartDate = "22.03.2023 16:00",
+                Name = "Künstliche Intelligenz PR",
+                Lecturer = "Krenz-Baath",
+                Room = "PC-Pool H4.2-E00-130",
+                Length = 120,
+                Biweekly = true
+            },
+
+
+
             };
 
         public static List<CourseViewModel> GetSearchResults(string queryString)
